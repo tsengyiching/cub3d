f688f9ef9bd5791c6fc0764d2fdaa3588b7506f6
Tseng Yi ching <yichingtsengtw@gmail.com>
2020-07-17 13:46:22 +0200

    reset draw walls

--- srcs/draw_walls.c
+++ srcs/draw_walls.c
@@ -6,67 +6,71 @@
 /*   By: yictseng <yictseng@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/14 14:20:36 by yictseng          #+#    #+#             */
-/*   Updated: 2020/07/16 20:31:32 by yictseng         ###   ########lyon.fr   */
+/*   Updated: 2020/07/17 13:45:18 by yictseng         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-// void	find_walls(t_mlx *mlx)
-// {
-// 	while (mlx->hit == 0)
-// 	{
-// 		if (mlx->side_distx < mlx->side_disty)
-// 		{
-// 			mlx->side_distx += mlx->delta_distx;
-// 			mlx->mapx += mlx->stepx;
-// 			mlx->side = 0;
-// 		}
-// 		else
-// 		{
-// 			mlx->side_disty += mlx->delta_disty;
-// 			mlx->mapy += mlx->stepy;
-// 			mlx->side = 1;
-// 		}
-// 		if (mlx->map[mlx->mapx][mlx->mapy] == '1')
-// 			mlx->hit = 1;
-// 	}
-// }
+void	find_walls(t_cfg *cfg, t_mlx *mlx)
+{
+	while (mlx->hit == 0)
+	{
+		if (mlx->side_distx < mlx->side_disty)
+		{
+			mlx->side_distx += mlx->delta_distx;
+			mlx->mapx += mlx->stepx;
+			mlx->side = 0;
+		}
+		else
+		{
+			mlx->side_disty += mlx->delta_disty;
+			mlx->mapy += mlx->stepy;
+			mlx->side = 1;
+		}
+		if (cfg->map[mlx->mapx][mlx->mapy] == '1')
+			mlx->hit = 1;
+	}
+}
 
-// void	calcul_perp_wall_dist(t_mlx *mlx)
-// {
-// 	if (mlx->side == 0)
-// 	{
-// 		mlx->perp_wall_dist = (mlx->mapx - mlx->posx +
-// 								(1 - mlx->stepx) / 2) / mlx->raydirx;
-// 	}
-// 	else
-// 	{
-// 		mlx->perp_wall_dist = (mlx->mapy - mlx->posy +
-// 								(1 - mlx->stepy) / 2) / mlx->raydiry;
-// 	}
-// }
+void	calcul_perp_wall_dist(t_mlx *mlx)
+{
+	if (mlx->side == 0)
+	{
+		mlx->perp_wall_dist = (mlx->mapx - mlx->posx +
+								(1 - mlx->stepx) / 2) / mlx->raydirx;
+	}
+	else
+	{
+		mlx->perp_wall_dist = (mlx->mapy - mlx->posy +
+								(1 - mlx->stepy) / 2) / mlx->raydiry;
+	}
+}
 
-// void	calcul_pixel_to_fill_in_stripe(t_mlx *mlx)
-// {
-// 	mlx->line_height = (int)(mlx->h / mlx->perp_wall_dist);
-// 	mlx->draw_start = -(mlx->line_height) / 2 + mlx->h / 2;
-// 	if (mlx->draw_start < 0)
-// 		mlx->draw_start = 0;
-// 	mlx->draw_end = mlx->line_height / 2 + mlx->h / 2;
-// 	if (mlx->draw_end >= mlx->h)
-// 		mlx->draw_end = mlx->h - 1;
-// }
+void	calcul_pixel_to_fill_in_stripe(t_mlx *mlx)
+{
+	mlx->line_height = (int)(mlx->height / mlx->perp_wall_dist);
+	mlx->draw_start = -(mlx->line_height) / 2 + mlx->height / 2;
+	if (mlx->draw_start < 0)
+		mlx->draw_start = 0;
+	mlx->draw_end = mlx->line_height / 2 + mlx->height / 2;
+	if (mlx->draw_end >= mlx->height)
+		mlx->draw_end = mlx->height - 1;
+}
 
-// void	draw_walls(int hor, t_mlx *mlx)
-// {
-// 	int ver;
+void	draw_walls(int hor, t_cfg *cfg, t_mlx *mlx)
+{
+	int ver;
 
-// 	ver = 0;
-// 	while (ver < mlx->height)
-// 	{
-// 		if (ver > mlx->draw_start && ver < mlx->draw_end)
-// 			mlx->pixel[hor + ver * mlx->width] = mlx->color;
-// 		ver++;
-// 	}
-// }
+	ver = 0;
+	while (ver < mlx->height)
+	{
+		if (ver < mlx->draw_start)
+			mlx->pixel[hor + ver * cfg->width] = cfg->ceiling;
+		if (ver >= mlx->draw_start && ver <= mlx->draw_end)
+			mlx->pixel[hor + ver * cfg->width] = mlx->color;
+		if (ver > mlx->draw_end)
+			mlx->pixel[hor + ver * cfg->width] = cfg->floor;
+		ver++;
+	}
+}
--- srcs/run_raycasting.c
+++ srcs/run_raycasting.c
@@ -6,7 +6,7 @@
 /*   By: yictseng <yictseng@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/10 15:51:32 by yictseng          #+#    #+#             */
-/*   Updated: 2020/07/17 12:41:27 by yictseng         ###   ########lyon.fr   */
+/*   Updated: 2020/07/17 13:45:31 by yictseng         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,69 +48,6 @@ void	calcul_step_and_init_sidedist(t_mlx *mlx)
 		mlx->stepy = 1;
 		mlx->side_disty = (mlx->mapy + 1.0 - mlx->posy)
 							* mlx->delta_disty;
-	}
-}
-
-void	find_walls(t_cfg *cfg, t_mlx *mlx)
-{
-	while (mlx->hit == 0)
-	{
-		if (mlx->side_distx < mlx->side_disty)
-		{
-			mlx->side_distx += mlx->delta_distx;
-			mlx->mapx += mlx->stepx;
-			mlx->side = 0;
-		}
-		else
-		{
-			mlx->side_disty += mlx->delta_disty;
-			mlx->mapy += mlx->stepy;
-			mlx->side = 1;
-		}
-		if (cfg->map[mlx->mapx][mlx->mapy] == '1')
-			mlx->hit = 1;
-	}
-}
-
-void	calcul_perp_wall_dist(t_mlx *mlx)
-{
-	if (mlx->side == 0)
-	{
-		mlx->perp_wall_dist = (mlx->mapx - mlx->posx +
-								(1 - mlx->stepx) / 2) / mlx->raydirx;
-	}
-	else
-	{
-		mlx->perp_wall_dist = (mlx->mapy - mlx->posy +
-								(1 - mlx->stepy) / 2) / mlx->raydiry;
-	}
-}
-
-void	calcul_pixel_to_fill_in_stripe(t_mlx *mlx)
-{
-	mlx->line_height = (int)(mlx->height / mlx->perp_wall_dist);
-	mlx->draw_start = -(mlx->line_height) / 2 + mlx->height / 2;
-	if (mlx->draw_start < 0)
-		mlx->draw_start = 0;
-	mlx->draw_end = mlx->line_height / 2 + mlx->height / 2;
-	if (mlx->draw_end >= mlx->height)
-		mlx->draw_end = mlx->height - 1;
-}
-
-void	draw_walls(int hor, t_cfg *cfg, t_mlx *mlx)
-{
-	int ver;
-
-	ver = 0;
-	while (ver < mlx->height)
-	{
-		if (ver < mlx->draw_start)
-			mlx->pixel[hor + ver * cfg->width] = cfg->ceiling;
-		if (ver >= mlx->draw_start && ver <= mlx->draw_end)
-			mlx->pixel[hor + ver * cfg->width] = mlx->color;
-		if (ver > mlx->draw_end)
-			mlx->pixel[hor + ver * cfg->width] = cfg->floor;
-		ver++;
 	}
 }
 
